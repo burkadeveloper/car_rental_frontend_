158e530ea63dab027b27d7a91ccf6c49ce6e1959
burka <burkaa16@gmail.com>
2026-07-23 17:58:17 +0300

    Remove duplicate default export in i18n.js

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -19,24 +19,3 @@ i18n
   });
 
 export default i18n;
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import en from "./locales/en/translation.json";
-import am from "./locales/am/translation.json";
-
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: { translation: en },
-      am: { translation: am },
-    },
-    fallbackLng: "en",
-    interpolation: {
-      escapeValue: false,
-    },
-  });
-
-export default i18n;
